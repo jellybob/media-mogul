98a95c84fe5cbfd35282b270745e019955c55061
Jon Wood <jon@blankpad.net>
2012-11-14 01:28:37 +0000

    Load metadata and filter out blacklisted files

--- src/media_mogul/core.clj
+++ src/media_mogul/core.clj
@@ -1,24 +1,20 @@
 (ns media-mogul.core
   [ :require [ clojure.java.io :as io ] ]
-  [ :require [ clojure.string :as string :only [ :split ]  ] ])
+  [ :require [ clojure.string :as string ] ])
 
 (def supported-files
   #{"mp4" "mkv" "avi"})
 
-(def media-path "/Volumes/shared/TV/")
+(def blacklist-patterns
+  [
+    #".*/\..*" ; Hidden files and directories
+    #".*\.sample.*" ; Samples
+  ])
 
-(defn files-in-path [ base_path ]
-  (map #(file-details %) (file-seq (io/file base_path))))
+(def media-path "/Volumes/shared/TV/")
 
 (defn file-extension [ path ]
   (last (string/split path #"\.")))
-
-(defn supported-file-type? [ file ]
-  (contains? supported-files (file :extension)))
-
-(defn find-media []
-  (filter supported-file-type? (files-in-path media-path)))
-
 
 (defn file-details [ file ]
  (let [ path (.getPath file) name (.getName file) extension (file-extension name) ]
@@ -28,22 +24,42 @@
    :extension extension
   }))
 
+(defn sanitized-path [ path ]
+  (string/replace-first path media-path "")
+  )
+
+(defn files-in-path [ base_path ]
+  (map #(file-details %) (file-seq (io/file base_path))))
+
+(defn supported-file-type? [ file ]
+  (contains? supported-files (file :extension)))
+
+(defn collect-matches? [ patterns s ]
+  (map #(re-matches % s) patterns))
+
+(defn ignored-path? [ path ]
+  (some #(not (nil? %)) (collect-matches? blacklist-patterns path)))
+
+(defn find-media []
+  (filter #(and (supported-file-type? %) (not (ignored-path? (% :path)))) (files-in-path media-path)))
+
 (def metadata-matchers [
-  #"(.*) - s(\d+)e(\d+) - (.*)" ; Series Name - s01e02 - Episode Name
-  #"(.*) - s(\d+)e(\d+) - (.*)" ; Series Name - s01_e02 - Episode Name
-  #"(.*)\.s(\d+)e(\d+)\.(.*)" ; Series.Name-s01e02-Episode.Name
-  #"(.*) \[(\d+)x(\d+)\] (.*)" ; Series Name [1x2] Episode Name
-  #"(.*)(\d+)x(\d+)(.*)" ; Series.Name.1x01.Episode.Name
+  #".*s(\d+)e(\d+).*" ; Series Name - s01e02 - Episode Name
+  #".*s(\d+)e(\d+).*" ; Series Name - s01_e02 - Episode Name
+  #".*\.s(\d+)e(\d+).*?" ; Series.Name-s01e02-Episode.Name
+  #".*\[(\d+)x(\d+)\].*?" ; Series Name [1x2] Episode Name
+  #".*(\d+)x(\d+).*?" ; Series.Name.1x01.Episode.Name
   ])
 
-(defn metadata [ filename ]
-  (def matches (map #(re-matches % (string/lower-case filename)) metadata-matchers))
-  (def actual-match (first (filter #(not (nil? %)) matches)))
+(defn metadata [ file ]
+  (let [
+      matches (map #(re-matches % (string/lower-case (file :filename))) metadata-matchers)
+      valid-matches (filter #(not (nil? %)) matches)
+      [ _ season episode ] (first valid-matches)
+    ]
 
-  (let [ [ _ series season episode title ] actual-match ]
-    {
-      :series series
+    (assoc file
+      :series (first (string/split (sanitized-path (file :path)) #"\/"))
       :season season
       :episode episode
-      :title title
-    }))
+    )))
