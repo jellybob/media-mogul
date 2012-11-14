3e08a09424ce5e3dde8339c179449d68bfd3738e
Jon Wood <jon@blankpad.net>
2012-11-14 10:53:24 +0000

    Hide some functions that shouldn't be public

--- src/media_mogul/library.clj
+++ src/media_mogul/library.clj
@@ -3,46 +3,46 @@
   [ :require [ clojure.java.io :as io ] ]
   [ :require [ clojure.string :as string ] ])
 
-(defn file-extension [ path ]
+(defn -file-extension [ path ]
   (last (string/split path #"\.")))
 
-(defn file-details [ file ]
- (let [ path (.getPath file) name (.getName file) extension (file-extension name) ]
+(defn -file-details [ file ]
+ (let [ path (.getPath file) name (.getName file) extension (-file-extension name) ]
   {
    :path path
    :filename (string/replace name (str "." extension) "")
    :extension extension
   }))
 
-(defn sanitized-path [ path ]
+(defn -sanitized-path [ path ]
   (string/replace-first path config/media-path ""))
 
-(defn files-in-path [ base_path ]
-  (map #(file-details %)
+(defn -files-in-path [ base_path ]
+  (map #(-file-details %)
    (file-seq (io/file base_path))))
 
-(defn supported-file-type? [ file ]
+(defn -supported-file-type? [ file ]
   (contains? config/supported-file-types (file :extension)))
 
-(defn collect-matches [ patterns s ]
+(defn -collect-matches [ patterns s ]
   (map #(re-matches % s) patterns))
 
-(defn blacklisted-path? [ path ]
+(defn -blacklisted-path? [ path ]
   (some #(not (nil? %))
-   (collect-matches config/blacklisted-paths path)))
+   (-collect-matches config/blacklisted-paths path)))
 
-(defn find-media []
+(defn -find-media []
   "Find any files that are both valid file types and not blacklisted"
   (filter #(and
-            (supported-file-type? %)
-            (not (blacklisted-path? (% :path))))
-   (files-in-path config/media-path)))
+            (-supported-file-type? %)
+            (not (-blacklisted-path? (% :path))))
+   (-files-in-path config/media-path)))
 
-(defn metadata [ file ]
+(defn -metadata [ file ]
   "Extract series and episode metadata from a file's path"
   (let [
-      path (sanitized-path (file :path))
-      matches (collect-matches config/metadata-matchers (string/lower-case (file :filename)))
+      path (-sanitized-path (file :path))
+      matches (-collect-matches config/metadata-matchers (string/lower-case (file :filename)))
       valid-matches (filter #(not (nil? %)) matches)
       [ _ season episode ] (first valid-matches)
     ]
@@ -52,3 +52,6 @@
       :season season
       :episode episode
     )))
+
+(defn metadata []
+  (map #(-metadata %) (-find-media)))
