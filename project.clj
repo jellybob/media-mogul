c51df6e24086f3eeaa0f832bd37d84fd8269160d
Jon Wood <jon@blankpad.net>
2012-11-15 20:37:09 +0000

    Kind of getting there

--- project.clj
+++ project.clj
@@ -4,6 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
+                 [clj-time "0.4.4"]
                  [org.lwjgl.lwjgl/lwjgl "2.8.4" ]
                  [org.clojars.jyaan/slick "247.1"]]
   :jvm-opts ["-Djava.library.path=./native"]
--- src/media_mogul/frontend.clj
+++ src/media_mogul/frontend.clj
@@ -1,12 +1,25 @@
-(ns media-mogul.frontend)
+(ns media-mogul.frontend
+  (:use [ clj-time.format :only (formatters unparse)]
+        [ clj-time.local :only (local-now) ]))
+
 (import '(org.newdawn.slick BasicGame GameContainer Graphics SlickException AppGameContainer))
 
-(def main-menu
-  (proxy [BasicGame] ["Frontend"]
-   (init [container])
-   (update [container delta])
-   (render [container graphics]
-    (.drawString graphics "Hello, world!" 0 100))))
+(defn clock-time []
+  (unparse (formatters :hour-minute-second) (local-now)))
 
-(defn start []
-  (.start (new AppGameContainer main-menu)))
+(def main-menu
+  (proxy [ BasicGame ] [ "Frontend" ]
+   (init [ container ])
+
+   (update [ container delta ])
+
+   (render [ container graphics ]
+    (.drawString graphics (clock-time) 30 (- 1024 150)))))
+
+(defn start [ options ]
+  (let [ menu (new AppGameContainer main-menu) ]
+   (.setDisplayMode menu
+    (get options :width 1024)
+    (get options :height 768)
+    (get options :fullscreen false))
+   (.start menu)))
