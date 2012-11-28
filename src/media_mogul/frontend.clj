e007540d70b897d87c33b0897059942bf577527f
Jon Wood <jon@blankpad.net>
2012-11-28 14:38:37 +0000

    Add some docs, privatise methods, and reorder the API sensibly.
    Not entirely sure about this one, but I think it'll do for now, at
    least until I know a bit more about how other people organise a
    Clojure API.

--- src/media_mogul/frontend.clj
+++ src/media_mogul/frontend.clj
@@ -3,34 +3,15 @@
 
 (import '(org.newdawn.slick AppGameContainer BasicGame))
 
-(def state
-  (ref { :view-ns 'media-mogul.frontend.main-menu
-         :show-fps false }))
+(declare state callback application-proxy set-view graphics-options)
 
-(def graphics-options {
-  :width 1024
-  :height 768
-  :fullscreen false
-  })
+(defn start
+ "Starts the GUI using the display options set in graphics-options.
 
-(defn set-view [ view-ns ]
-  (dosync
-    (alter state conj { :view-ns view-ns })))
+ This method will start an infinite event loop, so it should probably be
+ run in its own thread using (future (start))"
+ []
 
-(defn callback [ name & args ]
-  (let [ callback-fn @(name (ns-publics (the-ns (:view-ns @state)))) ]
-    (eval (apply callback-fn args))))
-
-(def application-proxy
-  (proxy [ BasicGame ] [ "Media Mogul" ]
-    (init [ container ])
-    (render [ container graphics ]
-      (callback 'render container graphics))
-    (update [ container delta ]
-      (.setShowFPS container (:show-fps @state))
-      (callback 'update container delta))))
-
-(defn start []
  (let [ container (new AppGameContainer application-proxy) ]
    (doto container
      (.setDisplayMode
@@ -41,3 +22,40 @@
      (.setAlwaysRender true)
      (.setTargetFrameRate 60)
      (.start))))
+
+(defn set-view
+  " Defines the namespace to look for update and render functions to display
+  the current state of the GUI. See media-mogul.frontend.example for an idea
+  of what that looks like"
+  [ view-ns ]
+
+  (dosync
+    (alter state conj { :view-ns view-ns })))
+
+(def graphics-options {
+  :width 1024
+  :height 768
+  :fullscreen false
+  })
+
+(def ^{ :private true }
+  application-proxy
+  (proxy [ BasicGame ] [ "Media Mogul" ]
+    (init [ container ])
+    (render [ container graphics ]
+      (callback 'render container graphics))
+    (update [ container delta ]
+      (.setShowFPS container (:show-fps @state))
+      (callback 'update container delta))))
+
+(def
+  ^{ :private true }
+  state
+  (ref { :view-ns 'media-mogul.frontend.main-menu
+         :show-fps false }))
+
+(defn
+  ^{ :private true }
+  callback [ name & args ]
+  (let [ callback-fn @(name (ns-publics (the-ns (:view-ns @state)))) ]
+    (eval (apply callback-fn args))))
